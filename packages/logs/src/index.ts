3e76373c87b679861537f10e19f50752b4a25595
sullyo <sully@cognosys.ai>
2025-02-19 00:06:23 -0800

    new packages

---
+++ packages/logs/src/index.ts
@@ -0,0 +1,38 @@
+import pino, { stdTimeFunctions } from "pino";
+
+interface LoggerOptions {
+  level?: string;
+  prettyPrint?: boolean;
+}
+
+function createLogger({
+  level = "info",
+  prettyPrint = false,
+}: LoggerOptions = {}) {
+  const options: pino.LoggerOptions = {
+    level,
+    formatters: {
+      level: (label) => {
+        return { level: label.toUpperCase() };
+      },
+    },
+    timestamp: stdTimeFunctions.isoTime,
+  };
+
+  if (prettyPrint) {
+    options.transport = {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+      },
+    };
+  }
+
+  return pino(options);
+}
+
+const logger = createLogger({
+  prettyPrint: true,
+});
+
+export { createLogger, logger };
