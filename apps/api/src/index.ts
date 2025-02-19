0e9b9a1d052f272b7e924eb8aeff2ed85639e2fd
sullyo <sully@cognosys.ai>
2025-02-19 10:25:32 -0800

    smol fix

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,12 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default { 
-  port: 3001, 
-  fetch: app.fetch, 
-} 
+export default {
+  port: 3004,
+  fetch: app.fetch,
+};
