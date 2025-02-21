bd073dca91dc27a075fb23b187309081afb18dfb
sullyo <sully@cognosys.ai>
2025-02-20 22:02:38 -0800

    db migrations

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -6,7 +6,7 @@ config({ path: ".env" });
 export default defineConfig({
   verbose: true,
   schemaFilter: ["public"],
-  schema: "./src/schema/index.ts",
+  schema: "./src/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
