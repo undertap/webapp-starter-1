ff94e6c6dc58f59bcd0745cc3b123deec818673d
sullyo <sully@cognosys.ai>
2025-02-21 22:49:06 -0800

    smal fix

--- apps/web/src/components/ui/chart.tsx
+++ apps/web/src/components/ui/chart.tsx
@@ -126,7 +126,7 @@ function ChartTooltipContent({
     }
 
     const [item] = payload;
-    const key = `${labelKey || item.dataKey || item.name || "value"}`;
+    const key = `${labelKey || item?.dataKey || item?.name || "value"}`;
     const itemConfig = getPayloadConfigFromPayload(config, item, key);
     const value =
       !labelKey && typeof label === "string"
